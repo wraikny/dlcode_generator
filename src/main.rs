c22b8b00d19cf4472d45d9b3b979f57197d8ff03
wraikny <writtenknown@gmail.com>
2018-12-18 07:56:02 +0900

    sample file

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use dlcode_generator::{file, code};
 
 fn main() {
     // Input and Output file name
-    let filename = "output/codes.txt";
+    let filename = "sample.txt";
     // The nubmer of new codes to generate
-    let gen_code_num = 50;
+    let gen_code_num = 10;
     let code_length = 16;
 
     let prev_codes : Vec<_> = 
